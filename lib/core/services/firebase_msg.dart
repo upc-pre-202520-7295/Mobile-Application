04408e49bc385d71e56d9362a9d67c87080163a1
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-11-13 03:16:31 -0500

    feat: implement Firebase messaging setup and token retrieval

---
+++ lib/core/services/firebase_msg.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FirebaseMsg {
+  final msgService = FirebaseMessaging.instance;
+
+  initFCM()async{
+    await msgService.requestPermission();
+    var token = await msgService.getToken();
+
+    print("✅✅✅✅✅✅✅✅Firebase Messaging Token: $token");
+
+    FirebaseMessaging.onBackgroundMessage(handleNotification);
+    FirebaseMessaging.onMessage.listen(handleNotification);
+
+  }
+
+}
+Future<void >handleNotification(RemoteMessage msg) async{
+}
